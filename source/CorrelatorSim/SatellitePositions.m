1ca4fda673a06e36e1053c9ddabf46efdd4c6c77
Noah Miller <nsm0014@auburn.edu>
2023-06-26 14:04:31 -0500

    calculated sv position velocities and clk corrections

--- source/CorrelatorSim/SatellitePositions.m
+++ source/CorrelatorSim/SatellitePositions.m
@@ -2,157 +2,191 @@ classdef SatellitePositions < handle
     %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
 
-    properties
+    properties (Access = public)
+       svPosX
+       svPosY
+       svPosZ
+       svVelX
+       svVelY
+       svVelZ
+       svClockCorr
+       unitVectors
+       transmitTime
+       transitTime
+       ID
 
+    end
+
+    properties (Access = private)
+        TGD
+        transmissionTime
+        svClockDriftRate
+        svClockDrift
+        svClockBias
+        Crc
+        Crs
+        Cuc
+        Cus
+        Cic
+        Cis
+        deltaN
+        M0
+        eccentricity
+        sqrtA
+        toe
+        omega0
+        i0
+        omegaDot
+        iDot
+    end
+
+    properties (Constant)
+        gpsPi = 3.1415926535898;        % Pi used in the GPS coordinate system
+        Omegae_dot = 7.2921151467e-5;   % Earth rotation rate, [rad/s]
+        GM = 3.986005e14;               % Earth's universal [m^3/s^2]
+        F = -4.442807633e-10;           % Constant, [sec/(meter)^(1/2)]
+        C = 299792458                   % Speed of light [m/s]
     end
 
     methods
-        function obj = SatellitePositions(eph,transmitTime,transitTime)
+        function obj = SatellitePositions(eph,transmitTime,position)
 
+            % Assign Properties from ephemeris file
+            obj.associateEph(eph)
 
+            % Initialize transitTime to be 68 ms for all satellites
+            transitTime = 0.068*ones([length(obj.toe) 1]);
 
-            obj.calcSVPositions(eph,transmitTime,transitTime);
+            for i = 1:10
+
+                % Calculate Satellite Positions
+                obj.calcSVPositions(transmitTime,transitTime);
+
+                % Recalculate Transit Time
+                svPos = [obj.svPosX obj.svPosY obj.svPosZ];
+                transitTime = vecnorm((svPos - position),2,2)/obj.C;
+
+            end
+
+            % Calculate user-sv unit vectors
+            obj.calcUnitVectors(position)
+
+            obj.transmitTime = transmitTime;
+            obj.transitTime = transitTime;
 
 
         end
-
     end
     methods (Access = private)
 
-        function calcSVPositions(obj,eph,transmitTime,transitTime)
+        function associateEph(obj,eph)
+            obj.TGD = eph.TGD;
+            obj.transmissionTime = eph.TransmissionTime;
+            obj.svClockDriftRate = eph.SVClockDriftRate;
+            obj.svClockDrift = eph.SVClockDrift;
+            obj.svClockBias = eph.SVClockBias;
+            obj.Crc = eph.Crc;
+            obj.Crs = eph.Crs;
+            obj.Cuc = eph.Cuc;
+            obj.Cus = eph.Cus;
+            obj.Cic = eph.Cic;
+            obj.Cis = eph.Cis;
+            obj.deltaN = eph.Delta_n;
+            obj.M0 = eph.M0;
+            obj.eccentricity = eph.Eccentricity;
+            obj.sqrtA = eph.sqrtA;
+            obj.toe = eph.Toe;
+            obj.omega0 = eph.OMEGA0;
+            obj.i0 = eph.i0;
+            obj.omegaDot = eph.OMEGA_DOT;
+            obj.iDot = eph.IDOT;
+            obj.ID = eph.SatelliteID;
+        end
 
-            T_GD=eph.TGD;
-            t_oc=eph.TransmissionTime;
-            a_f2=eph.SVClockDriftRate;
-            a_f1=eph.SVClockDrift;
-            a_f0=eph.SVClockBias;
-            C_rc=eph.Crc;
-            C_rs=eph.Crs;
-            C_uc=eph.Cuc;
-            C_us=eph.Cus;
-            C_ic=eph.Cic;
-            C_is=eph.Cis;
-            Delta_n=eph.Delta_n;
-            M_0=eph.M0;
-            e=eph.Eccentricity;
-            sqrt_A=eph.sqrtA; % Note this code assumes the data is A!!
-            t_oe=eph.Toe;
-            Omega_0=eph.OMEGA0;
-            i_0=eph.i0;
-            omega=eph.omega;
-            dot_Omega=eph.OMEGA_DOT;
-            Idot=eph.IDOT;
-            % GPS constatns
+        function calcSVPositions(obj,transmitTime,transitTime)
 
-            gpsPi = 3.1415926535898;  % Pi used in the GPS coordinate system
+            dt = obj.check_t(transmitTime - obj.transmissionTime);
 
-            %--- Constants for satellite position calculation -------------------------
-            Omegae_dot = 7.2921151467e-5;  % Earth rotation rate, [rad/s]
-            GM = 3.986005e14;      % Earth's universal [m^3/s^2]
-            F = -4.442807633e-10; % Constant, [sec/(meter)^(1/2)]
-
-
-            %--- Find time difference ---------------------------------------------
-            dt = obj.check_t(transmitTime - t_oc);
-
-            %--- Calculate clock correction ---------------------------------------
-            satClkCorr = (a_f2.*dt + a_f1).*dt + a_f0 - T_GD;
+            satClkCorr = (obj.svClockDriftRate.*dt + obj.svClockDrift).*dt + obj.svClockBias - obj.TGD;
 
             time = transmitTime - satClkCorr;
 
-            % Find satellite's position ----------------------------------------------
+            a = obj.sqrtA.^2;
 
-            %Restore semi-major axis (this is not needed if ephemeris is already given
-            % as "a" - In that case simply use:  a=semiMajAx);
-            a   = sqrt_A.*sqrt_A;
+            tk = obj.check_t(time - obj.toe);
 
-            %Time correction
-            tk  = obj.check_t(time - t_oe);
+            n0 = sqrt(obj.GM/a.^3);
 
-            %Initial mean motion
-            n0  = sqrt(GM/a.^3);
-            %Mean motion
-            n   = n0 + Delta_n;
+            n = n0 + obj.deltaN;
 
-            %Mean anomaly
-            M   = M_0 + n * tk;
+            M = obj.M0 + n*tk;
 
-            %Reduce mean anomaly to between 0 and 360 deg
-            M   = rem(M + 2*gpsPi, 2*gpsPi);
+            M = rem(M + 2*obj.gpsPi, 2*obj.gpsPi);
 
-            %Initial guess of eccentric anomaly
-            E   = M;
+            E = M;
 
-            %--- Iteratively compute eccentric anomaly ----------------------------
             for ii = 1:10
-                E_old = E;
-                E = M + e.*sin(E);
-                dE = rem(E - E_old, 2*gpsPi);
+                E = M + obj.eccentricity.*sin(E);
             end
 
-            %Reduce eccentric anomaly to between 0 and 360 deg
-            E = rem(E + 2*gpsPi, 2*gpsPi);
+            E = rem(E + 2*obj.gpsPi, 2*obj.gpsPi);
 
-            %Compute relativistic correction term
-            dtr = F.*e.*sqrt_A.*sin(E);
+            dtr = obj.F.*obj.eccentricity.*obj.sqrtA.*sin(E);
 
-            %Calculate the true anomaly
-            nu = atan2(sqrt(1 - e.^2).*sin(E), cos(E)-e);
+            nu = atan2(sqrt(1 - obj.eccentricity.^2).*sin(E), cos(E) - obj.eccentricity);
 
-            %Compute angle phi
-            phi = nu + omega;
+            phi = nu + obj.omega0;
 
-            %Reduce phi to between 0 and 360 deg
-            phi = rem(phi, 2*gpsPi);
+            phi = rem(phi, 2*obj.gpsPi);
 
-            %Correct argument of latitude
-            u = phi + C_uc.*cos(2*phi) + C_us.*sin(2*phi);
-            %Correct radius
-            r = a.*(1 - e.*cos(E)) + C_rc.*cos(2*phi) + C_rs.*sin(2*phi);
-            %Correct inclination
-            i = i_0 + Idot.*tk + C_ic.*cos(2*phi) + C_is.*sin(2*phi);
+            u = phi + obj.Cuc.*cos(2*phi) + obj.Cus.*sin(2*phi);
 
-            %Compute the angle between the ascending node and the Greenwich meridian
-            Omega = Omega_0 + (dot_Omega - Omegae_dot).*tk - Omegae_dot.*t_oe - Omegae_dot.*transitTime;
-            %Reduce to between 0 and 360 deg
-            Omega = rem(Omega + 2*gpsPi, 2*gpsPi);
+            r = a.*(1 - obj.eccentricity.*cos(E)) + obj.Crc.*cos(2*phi) + obj.Crs.*sin(2*phi);
+
+            incl = obj.i0 + obj.iDot.*tk + obj.Cic.*cos(2*phi) + obj.Cis.*sin(2*phi);
+
+            Omega = obj.omega0 + (obj.omegaDot - obj.Omegae_dot).*tk - obj.Omegae_dot.*obj.toe - obj.Omegae_dot.*transitTime;
+        
+            Omega = rem(Omega + 2*obj.gpsPi, 2*obj.gpsPi);
 
             X = r.*cos(u);
             Y = r.*sin(u);
 
-            %--- Compute satellite coordinates ------------------------------------
-            x = X.*cos(Omega) - Y.*cos(i).*sin(Omega);
-            y = X.*sin(Omega) + Y.*cos(i).*cos(Omega);
-            z = Y.*sin(i);
+            x = X.*cos(Omega) - Y.*cos(incl).*sin(Omega);
+            y = X.*sin(Omega) + Y.*cos(incl).*cos(Omega);
+            z = Y.*sin(incl);
 
-            %% SV velocity calculations
-            Edot = (n0' + Delta_n)./(1-e.*cos(E));
-            phidot = (sqrt(1 - e.^2)./(1 - e.*cos(E))).*Edot;
-            udot = (1 + 2.*C_us.*cos(2*phi) - 2*C_uc.*sin(2*phi)).*phidot;
-            rdot = 2*(C_rs.*cos(2*phi)-C_rc.*sin(2*phi)).*phidot + a.*e.*sin(E).*Edot;
-            idot = 2*(C_is.*cos(2*phi) - C_ic.*sin(2*phi)).*phidot + Idot;
+            Edot = (n0' + obj.deltaN)./(1-obj.eccentricity.*cos(E));
+
+            phidot = (sqrt(1 - obj.eccentricity.^2)./(1 - obj.eccentricity.*cos(E))).*Edot;
+
+            udot = (1 + 2.*obj.Cus.*cos(2*phi) - 2*obj.Cuc.*sin(2*phi)).*phidot;
+
+            rdot = 2*(obj.Crs.*cos(2*phi)-obj.Crc.*sin(2*phi)).*phidot + a.*obj.eccentricity.*sin(E).*Edot;
+
+            idot = 2*(obj.Cis.*cos(2*phi) - obj.Cic.*sin(2*phi)).*phidot + obj.iDot;
+
             Xdot = rdot.*cos(u) - r.*sin(u).*udot;
+
             Ydot = rdot.*sin(u) + r.*cos(u).*udot;
-            Omegadot = dot_Omega - Omegae_dot;
-            xdot = Xdot.*cos(Omega) - Ydot.*cos(i).*sin(Omega) + Y.*sin(i).*sin(Omega).*idot - y.*Omegadot;
-            ydot = Xdot.*sin(Omega) + Ydot.*cos(i).*cos(Omega) - Y.*sin(i).*cos(Omega).*idot + x.*Omegadot;
-            zdot = Ydot.*sin(i) + Y.*cos(i).*idot;
 
-            %%
+            deltaOmegaDot = obj.omegaDot - obj.Omegae_dot;
 
+            xdot = Xdot.*cos(Omega) - Ydot.*cos(incl).*sin(Omega) + Y.*sin(incl).*sin(Omega).*idot - y.*deltaOmegaDot;
 
-            %--- Compute satellite coordinates ------------------------------------
-            satPositions(1,:) = x;
-            satPositions(2,:) = y;
-            satPositions(3,:) = z;
+            ydot = Xdot.*sin(Omega) + Ydot.*cos(incl).*cos(Omega) - Y.*sin(incl).*cos(Omega).*idot + x.*deltaOmegaDot;
 
-            satVelocities(1,:) = xdot;
-            satVelocities(2,:) = ydot;
-            satVelocities(3,:) = zdot;
+            zdot = Ydot.*sin(incl) + Y.*cos(incl).*idot;
 
+            obj.svPosX = x;
+            obj.svPosY = y;
+            obj.svPosZ = z;
 
-            % Include relativistic correction in clock correction -----------------
-            satClkCorr = ((a_f2.*dt + a_f1).*dt + a_f0 - T_GD + dtr)';
+            obj.svVelX = xdot;
+            obj.svVelY = ydot;
+            obj.svVelZ = zdot;
+
+            obj.svClockCorr = ((obj.svClockDriftRate.*dt + obj.svClockDrift).*dt + obj.svClockBias - obj.TGD + dtr)';
 
         end
 
@@ -167,6 +201,22 @@ classdef SatellitePositions < handle
             elseif time < -half_week
                 corrTime = time + 2*half_week;
             end
+        end
+
+        function calcUnitVectors(obj,userPos)
+
+            svPos = [obj.svPosX obj.svPosY obj.svPosZ];
+            deltaPos = svPos - userPos;
+            range = vecnorm(deltaPos,2,2);
+
+            uX = deltaPos(:,1)./range;
+            uY = deltaPos(:,2)./range;
+            uZ = deltaPos(:,3)./range;
+
+            obj.unitVectors = [uX uY uZ];
+
+
+
         end
     end
 end
